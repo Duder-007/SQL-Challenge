e9029ac97788d81153c5f9881e63821208cbfe72
Duder-007 <159501857+Duder-007@users.noreply.github.com>
2024-05-31 10:20:54 -0700

    Query4

---
+++ EmployeeSQL/Script4.sql
@@ -0,0 +1,7 @@
+select distinct on (e.emp_no) e.emp_no, e.last_name, e.first_name,d.dept_name
+from employees as e
+left join dept_emp as de
+on e.emp_no = de.emp_no
+inner join departments as d
+on de.dept_no = d.dept_no
+order by e.emp_no DESC;
